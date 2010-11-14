e7e544d99c36ad7fd05b300d0562b40090175300
Randy Heydon <randy.heydon@clockworklab.net>
2010-11-13 23:18:33 -0500

    Replaced all tabs with spaces because of PEP 8.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(
-	name = 'distPND',
-	version = '0.0',
-	description = "Extensions to Python's Distutils to build PND files",
-	author = 'Randy Heydon',
-	author_email = 'randy.heydon@clockworklab.net',
-	packages = ['distpnd'],
-	license = 'MIT',
+    name = 'distPND',
+    version = '0.0',
+    description = "Extensions to Python's Distutils to build PND files",
+    author = 'Randy Heydon',
+    author_email = 'randy.heydon@clockworklab.net',
+    packages = ['distpnd'],
+    license = 'MIT',
 )
