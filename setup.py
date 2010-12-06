d270c388a9f4b0798dfd90433842842152aabd68
Randy Heydon <randy.heydon@clockworklab.net>
2010-12-06 17:48:16 -0500

    Minor fix to setup.py so PyPI sees readme.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'distPND',
     version = '0.1',
     description = "Extensions to Python's Distutils to build PND files",
-    long_description = open('README.rst'),
+    long_description = open('README.rst').read(),
     author = 'Randy Heydon',
     author_email = 'randy.heydon@clockworklab.net',
     packages = ['distpnd'],
